861308baebb3d3c9ab959435af7591e51b43401d
Mayasur <93368968+mayasur@users.noreply.github.com>
2022-06-27 12:58:28 +0530

    minor changes

--- src/components/Tasklist.jsx
+++ src/components/Tasklist.jsx
@@ -4,22 +4,39 @@ import Task from "./Task";
 import "./Tasklist.css"
 
 function Tasklist() {
-  const [data, setData] = useState([
-    { id: "1", title: "Clean kitchen" },
-    { id: "2", title: "Watch Tv" },
-  ]);
+  const [data, setData] = useState([]);
+
+
 
   const deleteTask = (id) => {
     setData(prev => prev.filter(d => d.id!==id))
     console.log(data)
   }
-  useEffect(() => {}, []);
+  useEffect(() => {
+
+    let fromlocal = localStorage.getItem("todo");
+    if(fromlocal !==null){
+      setData(JSON.parse(fromlocal))
+    }
+  }, []);
+  useEffect(() => {
+
+   localStorage.setItem("todo",JSON.stringify(data));
+  }, [data]);
 
   return (
     <div className="Tasklist">
-      {data.map((val) => {
+      {
+      data.length? (
+      data.map((val) => {
         return <Task val={val} key={val.id} deleteTask={deleteTask}/>;
-      })}
+      })
+      ):(
+        <p className="emptytext">Type a task below to view them&darr; </p>
+      )
+      
+      
+      }
       <Addtask data={data} setData={setData} />
     </div>
   );
